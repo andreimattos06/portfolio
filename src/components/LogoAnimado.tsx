e57d926935dd568c0bb0467ec78be96dd0d9ab88
Andrei dos Santos Mattos <andreimattos06@gmail.com>
2023-07-06 22:37:39 -0300

    First

---
+++ src/components/LogoAnimado.tsx
@@ -0,0 +1,23 @@
+export function LogoAnimado(){
+
+    return(
+        <>
+        <div className="flex flex-col">
+            <div className="flex">
+                <div className="text-3xl font-bold self-end animate-pulse text-defyellow">A</div>
+                <div className="self-center">ndrei</div>
+            </div>
+
+            <div className="flex pl-2 mt-[-16px]">
+                <div className="text-3xl font-bold animate-pulse text-defyellow">
+                    M
+                </div>
+                <div className="self-center">
+                    attos
+                </div>
+            </div>
+
+        </div>
+        </>
+    )
+}
